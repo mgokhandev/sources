e1e8277eb0e96eb34c1155316530c5bcf367c17c
Mehmet Gökhan <66775623+mgokhandev@users.noreply.github.com>
2021-09-20 10:00:32 +0300

    fuction TEST

--- day1/functions.js
+++ day1/functions.js
@@ -1,18 +1,20 @@
 function topla(a, b) {
-  return a + b;
+    return a + b;
 }
 
-const cikar = function (a, b) {
-  return a - b;
+const cikar = function(a, b) {
+    return a - b;
 };
 
 const carp = (a, b) => {
-  return a * b;
+    return a * b;
 };
 
 const bol = (a, b) => a / b;
 
 const merhabaDe = name => console.log(`Merhaba ${name}`);
+
+// const sayHello = name => console.log(`Hello ${name}`); TEST
 
 // let deger = topla(1, 5);
 // console.log(deger);
@@ -26,4 +28,15 @@ const merhabaDe = name => console.log(`Merhaba ${name}`);
 // let deger = bol(5, 1);
 // console.log(deger);
 
+// const deger = topla(2, 5); TEST
+// console.log(deger);
+
+// const deger = cikar(8, 7); TEST
+// console.log(deger);
+
+// let degerCarp = carp(3, 8); TEST
+// console.log(degerCarp);
+
 merhabaDe('Ahmet');
+
+// sayHello('Mehmet'); TEST
